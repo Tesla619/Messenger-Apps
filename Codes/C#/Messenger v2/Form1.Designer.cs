ec53b7e2a6990f3d3a446ee54117e0818ef1d831
Tesla619 <karl.spiteri.e21724@mcast.edu.mt>
2022-05-29 00:16:13 +0200

    Save

--- Codes/C#/Messenger v2/Form1.Designer.cs
+++ Codes/C#/Messenger v2/Form1.Designer.cs
@@ -38,12 +38,15 @@
             this.Send_Button = new System.Windows.Forms.Button();
             this.Main_TextBox = new System.Windows.Forms.RichTextBox();
             this.Server_Button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ID_Label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Port_TextBox
             // 
             this.Port_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.Port_TextBox.Location = new System.Drawing.Point(438, 37);
+            this.Port_TextBox.Location = new System.Drawing.Point(441, 65);
             this.Port_TextBox.Name = "Port_TextBox";
             this.Port_TextBox.Size = new System.Drawing.Size(46, 20);
             this.Port_TextBox.TabIndex = 19;
@@ -51,7 +54,7 @@
             // IP_TextBox
             // 
             this.IP_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.IP_TextBox.Location = new System.Drawing.Point(438, 11);
+            this.IP_TextBox.Location = new System.Drawing.Point(441, 39);
             this.IP_TextBox.Name = "IP_TextBox";
             this.IP_TextBox.Size = new System.Drawing.Size(91, 20);
             this.IP_TextBox.TabIndex = 18;
@@ -59,7 +62,7 @@
             // Sending_TextBox
             // 
             this.Sending_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.Sending_TextBox.Location = new System.Drawing.Point(8, 130);
+            this.Sending_TextBox.Location = new System.Drawing.Point(8, 152);
             this.Sending_TextBox.Name = "Sending_TextBox";
             this.Sending_TextBox.Size = new System.Drawing.Size(373, 33);
             this.Sending_TextBox.TabIndex = 17;
@@ -71,7 +74,7 @@
             this.Connect_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Connect_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Connect_Button.ForeColor = System.Drawing.Color.White;
-            this.Connect_Button.Location = new System.Drawing.Point(387, 73);
+            this.Connect_Button.Location = new System.Drawing.Point(387, 95);
             this.Connect_Button.Name = "Connect_Button";
             this.Connect_Button.Size = new System.Drawing.Size(91, 40);
             this.Connect_Button.TabIndex = 16;
@@ -85,7 +88,7 @@
             this.Port_Label.AutoSize = true;
             this.Port_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Port_Label.ForeColor = System.Drawing.Color.White;
-            this.Port_Label.Location = new System.Drawing.Point(396, 35);
+            this.Port_Label.Location = new System.Drawing.Point(397, 65);
             this.Port_Label.Name = "Port_Label";
             this.Port_Label.Size = new System.Drawing.Size(38, 20);
             this.Port_Label.TabIndex = 15;
@@ -97,7 +100,7 @@
             this.IP_Label.AutoSize = true;
             this.IP_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.IP_Label.ForeColor = System.Drawing.Color.White;
-            this.IP_Label.Location = new System.Drawing.Point(410, 11);
+            this.IP_Label.Location = new System.Drawing.Point(411, 39);
             this.IP_Label.Name = "IP_Label";
             this.IP_Label.Size = new System.Drawing.Size(24, 20);
             this.IP_Label.TabIndex = 14;
@@ -111,7 +114,7 @@
             this.Send_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Send_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Send_Button.ForeColor = System.Drawing.SystemColors.Window;
-            this.Send_Button.Location = new System.Drawing.Point(413, 130);
+            this.Send_Button.Location = new System.Drawing.Point(413, 152);
             this.Send_Button.Name = "Send_Button";
             this.Send_Button.Size = new System.Drawing.Size(106, 33);
             this.Send_Button.TabIndex = 13;
@@ -122,9 +125,9 @@
             // Main_TextBox
             // 
             this.Main_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.Main_TextBox.Location = new System.Drawing.Point(8, 10);
+            this.Main_TextBox.Location = new System.Drawing.Point(8, 12);
             this.Main_TextBox.Name = "Main_TextBox";
-            this.Main_TextBox.Size = new System.Drawing.Size(373, 103);
+            this.Main_TextBox.Size = new System.Drawing.Size(373, 123);
             this.Main_TextBox.TabIndex = 11;
             this.Main_TextBox.Text = "";
             // 
@@ -134,7 +137,7 @@
             this.Server_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Server_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Server_Button.ForeColor = System.Drawing.Color.White;
-            this.Server_Button.Location = new System.Drawing.Point(484, 73);
+            this.Server_Button.Location = new System.Drawing.Point(484, 95);
             this.Server_Button.Name = "Server_Button";
             this.Server_Button.Size = new System.Drawing.Size(53, 40);
             this.Server_Button.TabIndex = 20;
@@ -142,12 +145,45 @@
             this.Server_Button.UseVisualStyleBackColor = true;
             this.Server_Button.Click += new System.EventHandler(this.Server_Button_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(441, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(91, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // ID_Label
+            // 
+            this.ID_Label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ID_Label.AutoSize = true;
+            this.ID_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID_Label.ForeColor = System.Drawing.Color.White;
+            this.ID_Label.Location = new System.Drawing.Point(409, 13);
+            this.ID_Label.Name = "ID_Label";
+            this.ID_Label.Size = new System.Drawing.Size(26, 20);
+            this.ID_Label.TabIndex = 21;
+            this.ID_Label.Text = "ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "R";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.ClientSize = new System.Drawing.Size(540, 175);
+            this.ClientSize = new System.Drawing.Size(540, 197);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ID_Label);
             this.Controls.Add(this.Server_Button);
             this.Controls.Add(this.Port_TextBox);
             this.Controls.Add(this.IP_TextBox);
@@ -178,6 +214,9 @@
         private System.Windows.Forms.Button Send_Button;
         private System.Windows.Forms.RichTextBox Main_TextBox;
         private System.Windows.Forms.Button Server_Button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label ID_Label;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Codes/C#/Messenger v2/Form1.cs
+++ Codes/C#/Messenger v2/Form1.cs
@@ -17,11 +17,11 @@ namespace Messenger_v2
     {
         bool clientState = true;
         bool serverState = true;
-        public static TcpListener server;
-        public static TcpClient clientSocket = new TcpClient();
-        private static List<string> uniqueID = new List<string>();
+        NetworkStream clientStream;
+        static TcpListener server;
+        static TcpClient client = new TcpClient();
         string[] buttonTexts = { "Connect", "Disconnect", "Start", "Stop" };
-        
+
 
         public Form1()
         {
@@ -38,7 +38,7 @@ namespace Messenger_v2
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             server.Stop();
-            clientSocket.Close();
+            client.Close();
         }
 
         private static string GetLocalIP()
@@ -61,9 +61,9 @@ namespace Messenger_v2
 
             if (clientState == true)
             {
-                try 
+                try
                 {
-                    clientSocket.Close();
+                    client.Close();
                     Connect_Button.Text = buttonTexts[0];       // Connect               
                 }
                 catch (Exception ex) { clientState = Error_Message(clientState, Connect_Button, buttonTexts[1], ex); } // Disconnect
@@ -71,14 +71,15 @@ namespace Messenger_v2
             }
             else if (clientState == false)
             {
-                try 
+                try
                 {
                     Connect_Button.Text = buttonTexts[1]; // Disconnect
-                    clientSocket.Connect(IP_TextBox.Text, Convert.ToInt32(Port_TextBox.Text)); 
+                    client.Connect(IP_TextBox.Text, Convert.ToInt32(Port_TextBox.Text));                    
+                    //client = server.AcceptTcpClient();
                 }
                 catch (Exception ex) { clientState = Error_Message(clientState, Connect_Button, buttonTexts[0], ex); } // Connect
             }
-        }        
+        }
 
         private void Server_Button_Click(object sender, EventArgs e)
         {
@@ -118,160 +119,105 @@ namespace Messenger_v2
         private void Send_Button_Click(object sender, EventArgs e)
         {
             //
-            Main_TextBox.Text += "You Sent: " + Sending_TextBox.Text + " " + "\n";
+            clientStream = client.GetStream();
+            sendData(clientStream, Sending_TextBox.Text);            
+            Main_TextBox.Text += ID_Label.Text + "(You) Sent: " + Sending_TextBox.Text + " " + "\n";
             Sending_TextBox.Text = "";
         }
 
-        public static void Main(string[] args)
+        private void clientThread()
         {
-            #region Creating Objects for Network Communication
-            //TcpClient clientSocket = new TcpClient();
-            //TcpListener server = new TcpListener(IPAddress.Parse(IP), port);
-            #endregion
-
-            //server.Start();                                                                                             //Starting server to listen for tcp client connection requests
-            //Console.WriteLine("Server Started, waiting for a connection... \n");
-
-            while (true)                                                                                                        //main loop
+            try
             {
-                clientSocket = server.AcceptTcpClient();                                                                //waiting for a tcp client connection
-                //if (clientCounter < 0) clientCounter = 0;                                                                       //implemented to fix issue where counter would go below zero
-                //clientCounter++;
-                handleClient client = new handleClient();
-                string clientID = client.generateID();                                                                          //generating a random unique ID
+                #region Sending the ID to the Client
+                NetworkStream dataStream = client.GetStream();
+                sendData(dataStream, ID_Label.Text);
+                Console.WriteLine("ID sent, waiting for message... \n");
+                #endregion
 
-                //Console.WriteLine("Client number " + clientCounter + " has a client ID of " + clientID
-                //     + " Connected, on thread " + Thread.CurrentThread.ManagedThreadId + ".\n");
+                #region Main Loop: Receiving Data over the Network, Checking the ID & Responding Accordingly
+                while (client.Connected)
+                {
+                    String responseData = receiveData(dataStream);                                                          //Receiving the message from the client
 
-                client.startInitializing(clientSocket, clientID);//, clientCounter);                                                      //Initalzing the client object
+                    #region Splitting Message to check ID
+                    //var firstSplit = responseData.Split('[');
+                    //var finalSplit = firstSplit[1].Split(']');
+                    #endregion
+
+                    string recMsg = "Message: " + responseData + " received from ID " +
+                        ID_Label.Text + ".\n";
+
+                    Console.WriteLine(recMsg);
+                    sendData(dataStream, "ID Valid, Message Received.");
+
+                    #region Checking ID & responding back to client accordingly
+                    /*if (uniqueID.Contains(finalSplit[0]) == true)
+                    {
+                        string recMsg = "Message: " + finalSplit[1] + " received from ID " +
+                        finalSplit[0] + " on thread " + Thread.CurrentThread.ManagedThreadId + ".\n";
+
+                        Console.WriteLine(recMsg);
+                        sendData(dataStream, "ID Valid, Message Received.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ID was invalid connection closed.\n");
+                        sendData(dataStream, "ID was invalid closing connection.");
+                        Thread.Sleep(1000);
+                        closeClient();
+                    }*/
+                    #endregion
+                }
+                #endregion
+            }
+            catch (Exception e)                                                                                             //Warning that something went wrong and connection is closing
+            {
+                Console.WriteLine("\nClosing Connection to Client" + " due to error: " + e.ToString() + "\n");
+                closeClient();
             }
         }
 
-        public class handleClient
+        //Closing the Connection, decrementing the counter of clients and removing the uniqueID from the global list
+        private void closeClient()
         {
-            #region Creating Global Class Variables & TcpClient Object
-            TcpClient client = new TcpClient();
-            string clientID = "0";
-            int clientNum;
-            #endregion
+            Console.WriteLine("Client " + " with ID " + ID_Label.Text
+            + " Disconnected, on thread " + Thread.CurrentThread.ManagedThreadId + ".\n");
 
-            public void startInitializing(TcpClient inClientSocket, string inClientID)//, int inClientNum)
-            {
-                #region Inputting passed variables & TcpClient object to global ones
-                client = inClientSocket;
-                clientID = inClientID;
-                //clientNum = inClientNum;
-                #endregion
+            //clientCounter--;
+            //uniqueID.Remove(ID_Label.Text);
+            client.Close();
 
-                #region Multi Threading Code
-                //clientThread();                                                                                               //Used for testing before implementing multi-threading
-                Thread ctThread = new Thread(clientThread);                                                                     //Creating a new thread which will handle the particular client's I/O to the server
-                ctThread.Start();                                                                                               //Starting the thread
-                #endregion
-            }
+            //if (uniqueID.Count == 0) Console.WriteLine("Waiting for a connection... \n");                                   //To show that there is no active connection
+        }
 
-            private void clientThread()
+        //Returning sent string from over the network
+        private string receiveData(NetworkStream clientStream)
+        {
+            Byte[] recvData = new Byte[256];
+            Int32 bytesInt = clientStream.Read(recvData, 0, recvData.Length);
+            return Encoding.ASCII.GetString(recvData, 0, bytesInt);
+        }
+
+        //Sending a string over the network
+        private void sendData(NetworkStream clientStream, string sendString)
+        {
+            Byte[] sendData = new Byte[256];
+            sendData = Encoding.ASCII.GetBytes(sendString);
+            clientStream.Write(sendData, 0, sendData.Length);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            while(true) 
             {
                 try
                 {
-                    #region Sending the ID to the Client
-                    NetworkStream dataStream = client.GetStream();
-                    sendData(dataStream, clientID);
-                    Console.WriteLine("ID sent, waiting for message... \n");
-                    #endregion
-
-                    #region Main Loop: Receiving Data over the Network, Checking the ID & Responding Accordingly
-                    while (client.Connected)
-                    {
-                        String responseData = receiveData(dataStream);                                                          //Receiving the message from the client
-
-                        #region Splitting Message to check ID
-                        var firstSplit = responseData.Split('[');
-                        var finalSplit = firstSplit[1].Split(']');
-                        #endregion
-
-                        #region Checking ID & responding back to client accordingly
-                        if (uniqueID.Contains(finalSplit[0]) == true)
-                        {
-                            string recMsg = "Message: " + finalSplit[1] + " received from ID " +
-                            finalSplit[0] + " on thread " + Thread.CurrentThread.ManagedThreadId + ".\n";
-
-                            Console.WriteLine(recMsg);
-                            sendData(dataStream, "ID Valid, Message Received.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("ID was invalid connection closed.\n");
-                            sendData(dataStream, "ID was invalid closing connection.");
-                            Thread.Sleep(1000);
-                            closeClient();
-                        }
-                        #endregion
-                    }
-                    #endregion
+                    clientStream = client.GetStream();
+                    string msg = receiveData(clientStream);
                 }
-                catch (Exception e)                                                                                             //Warning that something went wrong and connection is closing
-                {
-                    //Console.WriteLine("\nClosing Connection to Client " + clientNum + " due to error: " + e.ToString() + "\n");
-                    Console.WriteLine("\nClosing Connection to Client" + " due to error: " + e.ToString() + "\n");
-                    closeClient();
-                }
-            }
-
-            //Generating a random unqiue ID, adding it to the global list of ID's and then returning it as a string
-            public string generateID()
-            {
-                #region Creating Method variable & object
-                Random rndGen = new Random();
-                bool unique = false;
-                #endregion
-
-                #region Generating ID & Checking it is unique
-                do
-                {
-                    int id = rndGen.Next(1, 10000);
-
-                    if (uniqueID.Contains(id.ToString("D4")) == false)
-                    {
-                        uniqueID.Add(id.ToString("D4"));
-                        unique = true;
-                    }
-
-                } while (unique == false);
-                #endregion
-
-                unique = false;                                                                                                 //Reseting variable
-
-                return uniqueID[uniqueID.Count - 1];
-            }
-
-            //Closing the Connection, decrementing the counter of clients and removing the uniqueID from the global list
-            private void closeClient()
-            {
-                Console.WriteLine("Client " + clientNum + " with ID " + clientID
-                + " Disconnected, on thread " + Thread.CurrentThread.ManagedThreadId + ".\n");
-
-                //clientCounter--;
-                uniqueID.Remove(clientID);
-                client.Close();
-
-                if (uniqueID.Count == 0) Console.WriteLine("Waiting for a connection... \n");                                   //To show that there is no active connection
-            }
-
-            //Returning sent string from over the network
-            private string receiveData(NetworkStream clientStream)
-            {
-                Byte[] recvData = new Byte[256];
-                Int32 bytesInt = clientStream.Read(recvData, 0, recvData.Length);
-                return Encoding.ASCII.GetString(recvData, 0, bytesInt);
-            }
-
-            //Sending a string over the network
-            private void sendData(NetworkStream clientStream, string sendString)
-            {
-                Byte[] sendData = new Byte[256];
-                sendData = Encoding.ASCII.GetBytes(sendString);
-                clientStream.Write(sendData, 0, sendData.Length);
+                catch (Exception ex) { }
             }
         }
     }
